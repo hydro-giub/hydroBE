1e7ac4dd532dd6538ed1774f6b834243a69ffde6
schiggo <simon.schick@giub.unibe.ch>
2016-01-11 16:10:43 +0100

    declusterSeries

---
+++ R/declusterSeries.R
@@ -0,0 +1,20 @@
+declusterSeries <-
+function(y=NULL,L=NULL) {
+  
+  n <- length(y)
+  d <- 1:n
+  if (n>2) {
+    i <- which(y[-c(1,n)]>y[-(1:2)] & y[-c(1,n)]>y[-c(n-1,n)])+1
+    ii <- which.max(y[i])
+    i <- i[ii]
+    t1 <- rep(F,n)
+    t1[d<(i-L+1)] <- T
+    t2 <- rep(F,n)
+    t2[d>(i+L-1)] <- T
+    ti1 <- declusterSeries(y=y[t1],L=L)
+    ti2 <- declusterSeries(y=y[t2],L=L)
+    ti <- c(ti1,i,ti2+sum(!t2))
+  } else {ti <- numeric(0)}
+  return(ti)
+  
+}
