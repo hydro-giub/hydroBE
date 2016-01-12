2512631e7c7d2883168e859324801779c1abdd24
schiggo <simon.schick@giub.unibe.ch>
2016-01-12 17:39:46 +0100

    skeleton importBafu

---
+++ R/importBafu.R
@@ -0,0 +1,6 @@
+importBafu <- function(dir=NULL,files=NULL) {
+  
+  #scan("/home/sschick/Downloads/RhoBri_Q_2346_Monatsmittel.asc",sep="\n",what=list(NULL),blank.lines.skip = F)
+  .hiddenF()
+  
+}
