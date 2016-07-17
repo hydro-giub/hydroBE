40e3ce0316a323558c5589f44e5e399e3f884f2d
schiggo <simon.schick@giub.unibe.ch>
2016-07-17 18:52:03 +0200

    finished pettitt from Pascal

---
+++ R/estimateBreakpointPettitt.R
@@ -0,0 +1,24 @@
+estimateBreakpointPettitt <- function(x,alpha=0.05) {
+  
+  n <- length(x)
+  m <- matrix(NA,nrow=n,ncol=3,dimnames=list(NULL,c('rank','cumsum','ut')))
+  
+  # test statistic u
+  m[,'rank'] <- rank(x)
+  m[,'cumsum'] <- cumsum(m[,'rank'])
+  m[,'ut'] <- 2*m[,'cumsum']-(1:n)*(n+1)
+  m[n,'ut'] <- NA
+  
+  # breakpoint k at index i
+  i <- which.max(abs(m[,'ut']))
+  k <- unname(abs(m[i,'ut'])) 
+  
+  # P(K>k)
+  p <- 2*exp((-6*(k^2))/(n^2+n^3))
+  
+  # k for which P(K>k) = alpha
+  l <- sqrt(-1/6*log(alpha/2,base=exp(1))*(n^2+n^3))
+  
+  return(list(index=i,p.value=round(p,3),k=k,critical.value=round(l,3),reject.H0=k>=l,u=m[,'ut']))
+  
+}
