dabb1b43e42e4eced3fb1ef011bc7c976c636c70
schiggo <simon.schick@giub.unibe.ch>
2017-07-12 17:50:30 +0200

    NA handling in estimateSnowFraction

--- R/estimateSnowFraction.R
+++ R/estimateSnowFraction.R
@@ -3,6 +3,7 @@ estimateSnowFraction <- function(y,thr=0.9,trans=1) {
     trans <- abs(trans)
     d <- dim(y)
     dim(y) <- NULL
+    y[is.na(y)] <- 9999
 
     f <- rep(0,length(y))
     f[y<=(thr-trans)] <- 1
@@ -12,6 +13,7 @@ estimateSnowFraction <- function(y,thr=0.9,trans=1) {
         f[i] <- (thr+trans-y[i])/(2*trans)
     }
 
+    f[y>9990] <- NA
     dim(f) <- d
     return(f)
     
