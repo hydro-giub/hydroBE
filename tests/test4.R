a13a3c39aff7bd514cb3c52221baf3076a63f0ab
schiggo <simon.schick@giub.unibe.ch>
2018-12-12 09:14:33 +0100

    improve breakpoint function test

--- tests/test4.R
+++ tests/test4.R
@@ -7,7 +7,7 @@ x <- rnorm(100)
 t1 <- testBreakpoint(x)
 t2 <- pettitt.test(x)
 
-tt <- c(t1$i==t2$estimate,
+tt <- c(t1$i==t2$estimate[1],
         round(t1$p.value,3)==round(t2$p.value,3),
         t1$k==t2$statistic)
 
