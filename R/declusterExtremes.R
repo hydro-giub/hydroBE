b55524c5cb7f08701547a1c4e3f46a51486b6888
simon <you@example.com>
2019-02-20 10:23:54 +0100

    make declusterExtremes apply()-conform

--- R/declusterExtremes.R
+++ R/declusterExtremes.R
@@ -1,19 +1,23 @@
-declusterExtremes <- function(y,L=11) {
+declusterExtremes <- function(x,L=11) {
     
-    n <- length(y)
+    n <- length(x)
     d <- 1:n
+    
     if(n>2) {
-        i <- which(y[-c(1,n)]>y[-(1:2)] & y[-c(1,n)]>y[-c(n-1,n)])+1
-        ii <- which.max(y[i])
+        i <- which(x[-c(1,n)]>x[-(1:2)] & x[-c(1,n)]>x[-c(n-1,n)])+1
+        ii <- which.max(x[i])
         i <- i[ii]
         t1 <- rep(F,n)
-        t1[d<(i-L+1)] <- T
+        t1[d<(i-L+1)] <- TRUE
         t2 <- rep(F,n)
-        t2[d>(i+L-1)] <- T
-        ti1 <- declusterExtremes(y=y[t1],L=L)
-        ti2 <- declusterExtremes(y=y[t2],L=L)
+        t2[d>(i+L-1)] <- TRUE
+        ti1 <- declusterExtremes(x=x[t1],L=L)
+        ti2 <- declusterExtremes(x=x[t2],L=L)
         ti <- c(ti1,i,ti2+sum(!t2))
-    } else {ti <- numeric(0)}
+    } else {
+        ti <- numeric(0)
+    }
+
     return(ti)
     
 }
