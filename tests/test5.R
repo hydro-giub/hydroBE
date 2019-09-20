53f068def74b357be7e2f6897d0793b26618272e
schiggo <schick87@gmail.com>
2019-09-20 11:21:37 +0200

    clean up

--- tests/test5.R
+++ tests/test5.R
@@ -4,10 +4,10 @@ o1 <- .Call(hydroBE:::runSum,x,as.integer(w))
 o2 <- filter(x,filter=rep(1,times=w),method='convolution',sides=1)
 
 tt <- is.na(o1[1:4])
-if(!all(tt)) {stop('testBreakpoint -- NA values')}
+if(!all(tt)) {stop('getRunSum -- NA values')}
 
 tt <- length(o1)==length(o2)
-if(!all(tt)) {stop('testBreakpoint -- vector length')}
+if(!all(tt)) {stop('getRunSum -- vector length')}
 
 tt <- round(o1[-(1:4)],5)==round(o2[-(1:4)],5)
-if(!all(tt)) {stop('testBreakpoint -- filter')}
+if(!all(tt)) {stop('getRunSum -- filter')}
